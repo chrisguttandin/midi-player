2a37166af51630128f80a62a3e6394c0e294a8c2
chrisguttandin <chrisguttandin@media-codings.com>
2016-05-25 14:57:28 +0200

    setup travis tests

--- config/karma/unit.js
+++ config/karma/unit.js
@@ -2,27 +2,57 @@
 
 module.exports = function (config) {
 
-    config.set({
+    /* eslint-disable indent */
+    var configuration = {
 
-        browsers: [
+            files: [
+                '../../test/unit/**/*.js'
+            ],
+
+            frameworks: [
+                'browserify',
+                'mocha',
+                'sinon-chai' // implicitly uses chai too
+            ],
+
+            preprocessors: {
+                '../../test/unit/**/*.js': 'browserify'
+            }
+
+        };
+    /* eslint-enable indent */
+
+    if (process.env.TRAVIS) {
+        configuration.browsers = [
+            'ChromeCanarySauceLabs',
+            'FirefoxDeveloperSauceLabs'
+        ];
+
+        configuration.captureTimeout = 120000;
+
+        configuration.customLaunchers = {
+            ChromeCanarySauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'chrome',
+                platform: 'OS X 10.11',
+                version: 'dev'
+            },
+            FirefoxDeveloperSauceLabs: {
+                base: 'SauceLabs',
+                browserName: 'firefox',
+                platform: 'OS X 10.11',
+                version: 'dev'
+            }
+        };
+
+        configuration.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+    } else {
+        configuration.browsers = [
             'ChromeCanary',
             'FirefoxDeveloper'
-        ],
+        ];
+    }
 
-        files: [
-            '../../test/unit/**/*.js'
-        ],
-
-        frameworks: [
-            'browserify',
-            'mocha',
-            'sinon-chai' // implicitly uses chai too
-        ],
-
-        preprocessors: {
-            '../../test/unit/**/*.js': 'browserify'
-        }
-
-    });
+    config.set(configuration);
 
 };
