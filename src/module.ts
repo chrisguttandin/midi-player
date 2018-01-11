c0cea06ef0f8a3c4211f2bc8b0f451846a565a87
chrisguttandin <chrisguttandin@media-codings.com>
2018-01-11 01:10:43 +0100

    style: apply linting rules

--- src/module.ts
+++ src/module.ts
@@ -6,13 +6,15 @@ import { PERFORMANCE_PROVIDER } from './providers/performance';
 import { WORKER_TIMERS_PROVIDER } from './providers/worker-timers';
 import { SCHEDULER_PROVIDER } from './scheduler';
 
-const injector = Injector.create([
-    MIDI_FILE_SLICER_FACTORY_PROVIDER,
-    MIDI_MESSAGE_ENCODER_PROVIDER,
-    MIDI_PLAYER_FACTORY_PROVIDER,
-    PERFORMANCE_PROVIDER,
-    SCHEDULER_PROVIDER,
-    WORKER_TIMERS_PROVIDER
-]);
+const injector = Injector.create({
+    providers: [
+        MIDI_FILE_SLICER_FACTORY_PROVIDER,
+        MIDI_MESSAGE_ENCODER_PROVIDER,
+        MIDI_PLAYER_FACTORY_PROVIDER,
+        PERFORMANCE_PROVIDER,
+        SCHEDULER_PROVIDER,
+        WORKER_TIMERS_PROVIDER
+    ]
+});
 
 export const midiPlayerFactory = injector.get(MidiPlayerFactory);
