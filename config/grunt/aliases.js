5e1617e25a37edacb6cfe52ab7c3f729fc5d2956
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-04-13 15:33:06 +0200

    build(config): update grunt config

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,5 +1,20 @@
+const { env } = require('process');
+
+// eslint-disable-next-line padding-line-between-statements
+const filter = (predicate, ...tasks) => (predicate ? tasks : []);
+const isTarget = (...targets) => env.TARGET === undefined || targets.includes(env.TARGET);
+const isType = (...types) => env.TYPE === undefined || types.includes(env.TYPE);
+
 module.exports = {
     build: ['sh:build'],
     lint: ['sh:lint-config', 'sh:lint-src', 'sh:lint-test'],
-    test: ['sh:test-expectation-chrome', 'sh:test-expectation-chrome-canary', 'sh:test-unit']
+    test: [
+        'build',
+        ...filter(
+            isType('expectation'),
+            ...filter(isTarget('chrome'), 'sh:test-expectation-chrome'),
+            ...filter(isTarget(), 'sh:test-expectation-chrome-canary')
+        ),
+        ...filter(isType('unit'), 'sh:test-unit')
+    ]
 };
