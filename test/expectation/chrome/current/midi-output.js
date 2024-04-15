5a23321ff05f7fa705aa6c4f8dc2d35d6a56f99b
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-04-15 23:37:36 +0200

    test: add link to Chromium issue

--- test/expectation/chrome/current/midi-output.js
+++ test/expectation/chrome/current/midi-output.js
@@ -1,6 +1,6 @@
 describe('MIDIOutput', () => {
     describe('clear()', () => {
-        // #1
+        // #1 https://issues.chromium.org/issues/40411677
 
         it('should not be implemented', () => {
             expect(MIDIOutput.prototype.clear).to.be.undefined;
